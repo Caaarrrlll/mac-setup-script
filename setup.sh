6112b92755b8958e13d8dd39ea054750e82c16c4
Francois Venter <venter109@gmail.com>
2019-12-30 09:55:49 +0200

    homebrew, wget, node, angular, nest, python

---
+++ setup.sh
@@ -0,0 +1,7 @@
+# install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install wget
+brew install node
+npm install -g @angular/cli @nestjs/cli
+export PATH="/usr/local/opt/python/libexec/bin:$PATH"
+brew install python
