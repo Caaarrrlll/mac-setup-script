7d0fb94a02b87bb7d333d6b29d450766e6260e32
Francois Venter <venter109@gmail.com>
2019-12-30 10:24:55 +0200

    export experimental code

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # mac-setup-script
-## A basic script that sets up a lot of the things I use in MacOS
+## A basic script that sets up a lot of the things I use in macOS
+### Gotcha's
+- The exports will be added for macOS Catalina / any other version of macOS if you have zsh shell installed
+
+### Software list
+- Homebrew
+- wget
+- NodeJS & npm
+- Angular & Nest Cli's
+- Python
--- setup.sh
+++ setup.sh
@@ -1,7 +1,20 @@
 # install homebrew
+echo "\e[92mInstalling Homebrew\e[39m"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+echo "\e[92mInstalling Git\e[39m"
+brew install git
+echo "\e[92mInstalling wget\e[39m"
 brew install wget
+echo "\e[92mInstalling NodeJS\e[39m"
 brew install node
+echo "\e[92mInstalling Angular & NestJS CLI\e[39m"
 npm install -g @angular/cli @nestjs/cli
-export PATH="/usr/local/opt/python/libexec/bin:$PATH"
+echo "\e[92mInstalling Python\e[39m"
+# Test this
+sed  '/export PATH="//usr/local/opt/python/libexec/bin:' ./zshrc
 brew install python
+echo "\e[92mMake Dev Folder\e[39m"
+mkdir ~/Development
+# echo "\e[92mInstall Flutter\e[39m"
+# git clone https://github.com/flutter/flutter.git ~/Development
+# export PATH="$PATH:`~/Development/flutter/bin"
