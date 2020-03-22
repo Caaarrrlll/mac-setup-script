f0af2e967759cddd14fdf6d3e017ff99e6b7f6e3
Francois <francois.venter@epiuse.com>
2020-03-22 16:51:20 +0200

    added latex installation

--- setup.sh
+++ setup.sh
@@ -16,18 +16,25 @@ case "${machine}" in
         # install homebrew
         echo "\e[92mInstalling Homebrew\e[39m"
         /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
         # echo "\e[92mInstalling Git\e[39m"
         # brew install git
         echo "\e[92mInstalling wget\e[39m"
         brew install wget
+
         echo "\e[92mInstalling oh-my-zsh\e[39m"
         sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-        # 2 very useful addons
         git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
         git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
         # echo "\e[92mInstalling nvm (Node Version Manager)\e[39m"
+
+        echo "\n[92mInstalling Latex for use with VS Code"
+        brew cask install mactex-no-gui
+        sudo tlmgr update --self && sudo tlmgr update --all
+
         echo "\e[92mInstalling Angular & NestJS CLI\e[39m"
         npm install -g @angular/cli @nestjs/cli firebase-tools
+
         # echo "\e[92mInstalling Python\e[39m"
         # Test this
         # sed  '/export PATH="//usr/local/opt/python/libexec/bin:' ./zshrc
