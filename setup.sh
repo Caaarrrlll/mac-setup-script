de0d19c014d60f1823bcef540fb0d4ad01e29ee1
Francois <francois.venter@epiuse.com>
2020-03-22 23:26:04 +0200

    Plugins update, flutter added

--- setup.sh
+++ setup.sh
@@ -17,8 +17,11 @@ case "${machine}" in
         echo "\e[92mInstalling Homebrew\e[39m"
         /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+        # git is installed on catalina
         # echo "\e[92mInstalling Git\e[39m"
         # brew install git
+
+        # Install wget for use
         echo "\e[92mInstalling wget\e[39m"
         brew install wget
 
@@ -26,10 +29,14 @@ case "${machine}" in
         sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
         git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
         git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+        sed 's/plugins=(/plugins=(zsh-autosuggestions '
+        sed 's/plugins=(/plugins=(zsh-syntax-highlighting '
         # echo "\e[92mInstalling nvm (Node Version Manager)\e[39m"
 
         echo "\n[92mInstalling Latex for use with VS Code"
         brew cask install mactex-no-gui
+        # test if this works in shell
+        . ~/.zshrc
         sudo tlmgr update --self && sudo tlmgr update --all
 
         echo "\e[92mInstalling Angular & NestJS CLI\e[39m"
@@ -39,13 +46,18 @@ case "${machine}" in
         # Test this
         # sed  '/export PATH="//usr/local/opt/python/libexec/bin:' ./zshrc
         # brew install python
+
         echo "\e[92mMake Dev Folder\e[39m"
         mkdir ~/Development
-        # echo "\e[92mInstall Flutter\e[39m"
-        # git clone https://github.com/flutter/flutter.git ~/Development
-        # cd flutter
-        # git checkout stable
-        # sed  '/export PATH="$PATH:`~/Development/flutter/bin"' ./zshrc
+
+        # Flutter
+        echo "\e[92mInstall Flutter\e[39m"
+        git clone https://github.com/flutter/flutter.git ~/Development/flutter
+        cd ~/Development/flutter
+        git checkout stable
+        echo '\nexport PATH=$PATH:~/Development/flutter/bin' >> ~/.zshrc
+        . ~/.zshrc
+        cd ~/
 esac
 
 
